89a3dc9c710fc1eff0cd7d8b3b839fba66a2f748
weicocogoat <97502018+weicocogoat@users.noreply.github.com>
2024-09-20 18:05:47 +0800

    Update serial_to_parallel.v

--- serial_to_parallel.v
+++ serial_to_parallel.v
@@ -26,28 +26,29 @@ module serial_to_parallel(
 // Currently will write only serial in to parallel out
     input clk,
     input rst,
-    input ctrl_en,
+    input ctrl_en,                      // Signals that data is coming in and should be processed
     
-    input addr_in,                  // Serial address input
-    input data_in,                  // Serial data input (for writing)
-    input ctrl,                     // Control signal to determine the operation (reading ort writing)
+    input addr_in,                      // Serial address input
+    input data_in,                      // Serial data input (for writing)
+    //input ctrl,                       // Control signal to determine the operation (reading ort writing). 
+                                        // ctrl Likely not needed now cuz I am implementing a top module controller that does this
     
-    output [19:0] addr_out,         // Address sent to MRAM
-    output [15:0] data_out,          // Serves as data input when reading from MRAM, Serves as data output when writing to RAM
-    output reg chip_en,                 // Chip enable
-    output reg write_en,                // Write enable
-    output reg out_en,                  // Read enable
-    output reg lower_byte_en,           // Reading of bytes 7:0 enable line
-    output reg upper_byte_en            // Reading of bytes 15:8 enable line
+    output [19:0] addr_out,             // Address sent to MRAM
+    output [15:0] data_out,             // Serves as data input when reading from MRAM, Serves as data output when writing to RAM
+    output reg chip_en,                 // Chip enable, active low
+    output reg write_en,                // Write enable, active low
+    output reg out_en,                  // Read enable, active low
+    output reg lower_byte_en,           // Reading of bytes 7:0 enable line, active low
+    output reg upper_byte_en            // Reading of bytes 15:8 enable line, active low
 );
 
 // Variables
 reg [19:0] addr_shift_reg;
 reg [15:0] data_shift_reg;
-reg [4:0] counter;          // 5 flip flops to use as a 5-bit counter. Need to count up to 20. 
-reg addr_en;                // Addr enable signal
-reg data_en;                // Data enable signal
-reg send_data;              // Flag to send parallel data out
+reg [4:0] counter;                      // 5 flip flops to use as a 5-bit counter. Need to count up to 20. 
+reg addr_en;                            // Addr enable signal
+reg data_en;                            // Data enable signal
+reg send_data;                          // Flag to send parallel data out
 
 
 // Datapath
@@ -81,11 +82,10 @@ begin
     end
     else begin
         if (ctrl_en) begin
-            counter <= counter + 1;         // Increment counter by 1 for every clock edge
             data_en <= data_en;         
             addr_en <= addr_en;         
             case (counter)
-                5'd1  : begin               // Keep track of the number of bits being shifted into data and addr shift registers
+                5'd0  : begin               // Keep track of the number of bits being shifted into data and addr shift registers
                         data_en <= 1; 
                         addr_en <= 1;
                         end
@@ -95,31 +95,34 @@ begin
                 5'd20 : addr_en <= 0;       // All 20 bits have been shifted into the shift register, stop the shift and retain current state
                 
                 5'd21 : begin               
-                        send_data <= 1;     // At the 21st cycle, both addr and data shift registers are full and they can be moved to the output
                         counter <= 0;       // Reset counter to 0 and prepare for the next set of inputs
-                        chip_en <= 1;       
-                        write_en <= 1;               
-                        out_en <= 0;                
-                        lower_byte_en <= 1;    
-                        upper_byte_en <= 1;
+                        send_data <= 1;     // At the 21st cycle, both addr and data shift registers are full and they can be moved to the output
+                        chip_en <= 0;       //
+                        write_en <= 0;               
+                        out_en <= 1;                
+                        lower_byte_en <= 0;    
+                        upper_byte_en <= 0;
                         end
                         
                 default : begin
-                          send_data <= 0;   // At every other clock cycle, do not send the data over yet.
-                          chip_en <= 0;       
-                          write_en <= 0;               
-                          out_en <= 0;                
-                          lower_byte_en <= 0;    
-                          upper_byte_en <= 0;
+                          send_data <= 0;   // At every other clock cycle, do not send the data over yet
+                          chip_en <= 1;     // chip_en also helps to prevent the MRAM from reading the data as valid  
+                          write_en <= 1;               
+                          out_en <= 1;                
+                          lower_byte_en <= 1;    
+                          upper_byte_en <= 1;
                           end
-            endcase
+             endcase
+             counter <= counter + 1;         // Increment counter by 1 for every clock edge
         end
     end
 end
 
-//assign addr_out = (send_data) ? addr_shift_reg : 0;
-//assign data_out = (send_data) ? data_shift_reg : 0;
+// If send_data gets asserted, send the contents of the shift registers out. If not, retain its old value. 
+//assign addr_out = (send_data) ? addr_shift_reg : addr_out;
+//assign data_out = (send_data) ? data_shift_reg : data_out;
 
+// Use this for debugging purposes. For implementation, use the above with tenary operator
 assign addr_out = addr_shift_reg;
 assign data_out = data_shift_reg;
 
