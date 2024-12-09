85d7e414104d857b88448a5b7630d78b2d09dc16
weicocogoat <97502018+weicocogoat@users.noreply.github.com>
2024-12-09 16:45:19 +0800

    Add files via upload

---
+++ Testing MRAM with STM32F411RE/SPS_Burst_Test/Tang_Nano_files/delay_register.v
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 17.11.2024 17:33:50
+// Design Name: 
+// Module Name: delay_register
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module delay_register(
+    input clk,
+    input rst,
+    
+    input in,
+    
+    output out
+);
+
+reg temp;
+
+always @(posedge clk or posedge rst)
+begin
+    if (rst) begin
+        temp <= 0;
+    end
+    else begin
+        temp <= in;
+    end
+end
+
+assign out = temp;
+endmodule
