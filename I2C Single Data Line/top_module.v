f213e255e8f8745455315a97ec5bd52716ad5ff8
weicocogoat <97502018+weicocogoat@users.noreply.github.com>
2024-12-14 01:39:35 +0800

    Add files via upload
    Included simulated MRAM to test the design

--- I2C Single Data Line/top_module.v
+++ I2C Single Data Line/top_module.v
@@ -22,22 +22,25 @@
 
 module top_module(
     input sda,
-    input scl,
-    
-    output [15:0] write_data,
-    output [19:0] write_addr,
-    
-    output chip_en, 
-    output read_en,
-    output write_en,
-    output lb_en,
-    output ub_en
+    input scl
 );
 
 wire PTS_en;
 wire [7:0] index;
 
 wire ser_data_input;
+
+// Simulate with MRAM
+wire [15:0] write_data;
+wire [19:0] write_addr;
+    
+wire chip_en;
+wire read_en;
+wire write_en;
+wire lb_en;
+wire ub_en;
+
+wire [15:0] data_out;
 
 I2C_Simple_Slave_Controller uut1
 (
@@ -65,10 +68,24 @@ PTS_module uut2
     .PTS_en(PTS_en),
     .index(index),
     
-    .data_in(data_in),
+    .data_in(data_out),
     
     .ser_data_out(ser_data_input)
 );
+
+MRAM_model uut3
+(
+    .clk(scl),
     
+    .e_chipEnable_n(chip_en),
+    .g_outputEnable_n(read_en),
+    .w_writeEnable_n(write_en),
+    .lb_lowerByteEnable_n(lb_en),
+    .ub_upperByteEnable_n(ub_en),
+    
+    .address(write_addr),
+    .dqi_datainput(write_data),
+    .dqo_dataoutput(data_out)
+);
 
 endmodule
---
+++ I2C Single Data Line/top_module_tb.v
@@ -0,0 +1,153 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 14.12.2024 01:19:57
+// Design Name: 
+// Module Name: top_module_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top_module_tb;
+
+reg sda = 1;
+reg scl = 1;
+
+top_module uut
+(
+    .sda(sda),
+    .scl(scl)
+);
+
+integer i, j;
+reg foo;
+
+always begin
+    #5
+    scl = ~scl;
+end
+
+initial begin
+    // When SCL is high, pull SDA Low
+    sda <= 1;
+    #1
+    sda <= 0;
+    #1      // For some reason, this must be included or else I2C fails for some reason
+
+    // Input slave address and read_write sel
+    for (i = 0; i < 7; i = i+1) begin
+        @(negedge scl)
+        sda <= 1;
+        #1 
+        sda <= 1;
+    end
+    
+    /*
+    // Read operation
+    @(negedge scl)
+    sda <= 0;
+    #1 
+    sda <= 0;
+    */
+    
+    
+    // Write operation
+    @(negedge scl)
+    sda <= 1;
+    #1 
+    
+   
+    // ack signal from master
+    @(negedge scl)
+    sda <= 1;
+    #1 
+    sda <= 1;
+    
+    
+    // Input addr and burst len
+    // First 16 bits of addr
+    for (j = 0; j < 2; j = j+1) begin
+        for (i = 0; i < 8; i = i+1) begin
+            @(negedge scl)
+            sda <= 0;
+            #1 
+            sda <= 0;
+        end
+        
+        // ack signal from master
+        @(negedge scl)
+        sda <= 0;
+        #1 
+        sda <= 0;
+    end
+    
+    // Burst len of 2 - b'0010
+    @(negedge scl)
+    sda <= 0;
+    #1 
+    sda <= 0;
+    
+    @(negedge scl)
+    sda <= 0;
+    #1 
+    sda <= 0;
+    
+    @(negedge scl)
+    sda <= 1;
+    #1 
+    sda <= 1;
+    
+    @(negedge scl)
+    sda <= 0;
+    #1 
+    sda <= 0;
+    
+    // Remaining 4 bits of addr
+    for (i = 0; i < 4; i = i+1) begin
+        @(negedge scl)
+        sda <= 0;
+        #1 
+        sda <= 0;
+    end
+    
+    // ack signal from master
+    @(negedge scl)
+    sda <= 1;
+    #1 
+    sda <= 1;
+    
+    
+    
+    // Input 
+    for (j = 0; j < 2; j = j+1) begin
+        for (i = 0; i < 8; i = i+1) begin
+            @(negedge scl)
+            sda <= i%2;
+            #1 
+            sda <= i%2;
+        end
+        
+        @(negedge scl)
+        #1 
+        sda <= sda + 1;
+    end
+    
+    #500
+    
+    $finish;
+    
+end
+
+endmodule
