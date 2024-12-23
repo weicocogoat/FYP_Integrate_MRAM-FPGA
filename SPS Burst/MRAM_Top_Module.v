0b25df88bc261c9bb9eaab7a1fc2e6815dcc1e50
weicocogoat <97502018+weicocogoat@users.noreply.github.com>
2024-12-24 00:50:34 +0800

    Update MRAM_Top_Module.v
    updated to suit zedboard implementation

--- SPS Burst/MRAM_Top_Module.v
+++ SPS Burst/MRAM_Top_Module.v
@@ -28,8 +28,14 @@ module MRAM_Top_Module(
     input addr_in,
     input [2:0] read_write_sel,
     
-    output [15:0] data_out,
+    inout [15:0] data_out,
     output [19:0] addr_out,
+    
+    output chip_en,                 // Chip enable, active low
+    output write_en,                // Write enable, active low
+    output out_en,                  // Read enable, active low
+    output lower_byte_en,           // Reading of bytes 7:0 enable line, active low
+    output upper_byte_en,            // Reading of bytes 15:8 enable line, acti
     
     //input [15:0] parallel_data_in,
     output ser_data_out
@@ -43,10 +49,14 @@ wire send_data;
 wire load;                    // Flag to load data from MRAM to internal shift registers
 wire data_in_from_MRAM_en;    // Enable the data in from MRAM PTS module, Bit 0 enables full word, Bit 1 enables half word
 
-wire [15:0] parallel_data_in;
+//wire [15:0] parallel_data_in;
 
 wire [1:0] prev_read_write_sel;
 
+wire out_en_intermediate;
+wire [15:0] data;
+
+/*
 wire chip_en;                 // Chip enable, active low
 wire write_en;                // Write enable, active low
 wire out_en;                  // Read enable, active low
@@ -66,6 +76,7 @@ MRAM_model MRAM(
     .dqi_datainput(data_out),
     .dqo_dataoutput(parallel_data_in)
 );
+*/
 
 serial_to_parallel #(.BUS_WIDTH(16)) data_STP
 (
@@ -76,7 +87,7 @@ serial_to_parallel #(.BUS_WIDTH(16)) data_STP
     .data_in(data_in),
     .send_data(send_data),                                     
            
-    .data_out(data_out)          
+    .data_out(data)          
 );
 
 serial_to_parallel #(.BUS_WIDTH(20)) addr_STP
@@ -102,7 +113,7 @@ parallel_to_serial #(.BUS_WIDTH(16)) PTS
     
     .word_sel(prev_read_write_sel),  // Bits to select full, upper or lower byte
     
-    .data_in(parallel_data_in),           // Data from MRAM
+    .data_in(data_out),           // Data from MRAM
     
     .data_out(ser_data_out)
 );
@@ -125,9 +136,12 @@ control_module controller
     
     .chip_en(chip_en),                 
     .write_en(write_en),              
-    .out_en(out_en),                 
+    .out_en(out_en_intermediate),                 
     .lower_byte_en(lower_byte_en),           
     .upper_byte_en(upper_byte_en)          
 );
+
+assign out_en = out_en_intermediate;
+assign data_out = (out_en_intermediate) ? data : 'bz;
 
 endmodule
